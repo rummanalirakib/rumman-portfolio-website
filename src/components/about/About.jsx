cfaa6bb67b2b6dd2db1f8fdc1df09b23d565ec8c
LAPTOP-KIOKN62L\User <rummanalirakib11@gmail.com>
2024-11-23 20:24:07 -0500

    Remove services, add 10% of portfolio and completed experience section.

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,59 @@
 import React from 'react'
 import './About.css'
+import Myself from '../../assets/rumman_profile.jpg'
+import { MdOutlineWork } from "react-icons/md"
+import { TbUsersGroup } from "react-icons/tb"
+import { GoProjectSymlink } from "react-icons/go";
 
 const About = () => {
   return (
-    <section id="about">About</section>
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+      
+      <div className='container about__container'>
+        <div className='about__me'>
+            <div className='about__me-image'>
+              <img src={Myself} alt="About Me"/>
+
+            </div>
+        </div>
+
+        <div className='about__content'>
+         <div className='d-flex'>
+            <div className="about__cards">
+               <article className='about__card'>  
+                  <MdOutlineWork className='about-icon'/>
+                  <h5>Experience</h5>
+                  <small>1.5+ Years Working</small>
+               </article>
+            </div>
+
+            <div className="about__cards">
+               <article className='about__card'>  
+                  <TbUsersGroup className='about-icon'/>
+                  <h5>Clients</h5>
+                  <small>200+ Worldwide</small>
+               </article>
+            </div>
+
+            <div className="about__cards">
+               <article className='about__card'>  
+                  <GoProjectSymlink className='about-icon'/>
+                  <h5>Projects</h5>
+                  <small>80+ Completed</small>
+               </article>
+            </div>
+         </div>
+            <p>
+               Detail-oriented and results-driven Software Engineer and Data Scientist with 1.5+ years of experience in software development, machine learning, and data analysis. Skilled in Python, Java, and web development frameworks, with a 
+               focus on leveraging machine learning models to drive insightful decision-making. Proficient in working within Agile environments, collaborating with cross-functional teams to deliver high-quality solutions. Passionate about enhancing 
+               software systems, particularly in the context of issue resolution, collaboration networks, and predictive analytics. 
+            </p>
+            <a href="#contact" className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,20 @@ import './Portfolio.css'
 
 const Portfolio = () => {
   return (
-    <section id="portfolio">Portfolio</section>
+    <section id="portfolio">
+      <h5>My Recent Projects</h5>
+      <h2>Portfolio</h2>
+
+      <div className='container portfolio__container'>
+        <article className='portfolio__item'>
+            <div className='portfolio__item-image'>
+
+            </div>
+            <h3>This is a portfolio item title</h3>
+            <a href="https://github.com/rummanalirakib/Digital-Logic-Design-Simulator" className='btn'>GitHub</a>
+        </article>
+      </div>
+    </section>
   )
 }
 
