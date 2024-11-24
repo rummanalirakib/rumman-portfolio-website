985f6a56d91ccc7ca854f83a483affd1e1b2e36b
LAPTOP-KIOKN62L\User <rummanalirakib11@gmail.com>
2024-11-24 17:57:43 -0500

    Completed Portfolio Section

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,50 @@
 import React from 'react'
 import './Portfolio.css'
+import IMG1 from '../../assets/DLD Simulator.png'
+import IMG2 from '../../assets/DLD Simulator.png'
+import IMG3 from '../../assets/DLD Simulator.png'
+import IMG4 from '../../assets/DLD Simulator.png'
+import IMG5 from '../../assets/DLD Simulator.png'
+import IMG6 from '../../assets/DLD Simulator.png'
+
+const data = [
+  {
+    id: 1,
+    image: IMG1,
+    title: 'Digital Logic Design Simulator',
+    github: 'https://github.com/rummanalirakib/Digital-Logic-Design-Simulator'
+  },
+  {
+    id: 2,
+    image: IMG2,
+    title: 'Digital Logic Design Simulator',
+    github: 'https://github.com/rummanalirakib/Digital-Logic-Design-Simulator'
+  },
+  {
+    id: 3,
+    image: IMG3,
+    title: 'Digital Logic Design Simulator',
+    github: 'https://github.com/rummanalirakib/Digital-Logic-Design-Simulator'
+  },
+  {
+    id: 4,
+    image: IMG4,
+    title: 'Digital Logic Design Simulator',
+    github: 'https://github.com/rummanalirakib/Digital-Logic-Design-Simulator'
+  },
+  {
+    id: 5,
+    image: IMG5,
+    title: 'Digital Logic Design Simulator',
+    github: 'https://github.com/rummanalirakib/Digital-Logic-Design-Simulator'
+  },
+  {
+    id: 6,
+    image: IMG6,
+    title: 'Digital Logic Design Simulator',
+    github: 'https://github.com/rummanalirakib/Digital-Logic-Design-Simulator'
+  }
+]
 
 const Portfolio = () => {
   return (
@@ -8,13 +53,19 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className='container portfolio__container'>
-        <article className='portfolio__item'>
-            <div className='portfolio__item-image'>
-
-            </div>
-            <h3>This is a portfolio item title</h3>
-            <a href="https://github.com/rummanalirakib/Digital-Logic-Design-Simulator" className='btn'>GitHub</a>
-        </article>
+        {
+          data.map(({id, image, title, github}) => {
+            return (
+              <article key={id} className='portfolio__item'>
+                <div className='portfolio__item-image'>
+                    <img src={image} alt={title} />
+                </div>
+                <h3>{title}</h3>
+                <a href={github} className='btn'>GitHub</a>
+              </article>
+            )
+          })
+        }
       </div>
     </section>
   )
